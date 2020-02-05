2828f932db9005edda11d467e6a0c895e618b9c6
Max <you@example.com>
2020-02-05 11:12:41 -0800

    add web page server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+const port = 1501
+let counter = 0
+
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => res.send('Hello MAX'))
+
+app.get('/counter', function (req, res) {
+	counter = counter + 1
+  res.send('Counter ' + counter)
+})
+
+app.get('/date', function (req, res) {
+	let d = new Date();
+  res.send('date ' + d)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
